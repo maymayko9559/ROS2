4e3e3a791230c18d03c6f80b2f939acd22c4552d
maymayko9559 <maymayko9559@gmail.com>
2025-07-11 14:42:51 -0500

    create new robot pkg

---
+++ ros2_ws/build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/may/ros2_study/ros2_ws/src/my_robot_description
+# Build directory: /home/may/ros2_study/ros2_ws/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
