27a3ad6bc5b886dd9c8174612f14b386be6b67e5
maymayko9559 <maymayko9559@gmail.com>
2025-08-03 10:55:37 -0500

    preempt current goal if new goal received.

--- ros2_ws/src/actions_py/actions_py/count_until_server.py
+++ ros2_ws/src/actions_py/actions_py/count_until_server.py
@@ -26,15 +26,22 @@ class CountUntilServerNode(Node):
         self.get_logger().info("Received a goal")
 
         # Policy: refuse new goal if current goal still active
-        with self.goal_lock_:
-            if self.goal_handle_ is not None and self.goal_handle_.is_active:
-                self.get_logger().info("A goal is already active, rejecting new goal.")
-                return GoalResponse.REJECT
+        # with self.goal_lock_:
+        #     if self.goal_handle_ is not None and self.goal_handle_.is_active:
+        #         self.get_logger().info("A goal is already active, rejecting new goal.")
+        #         return GoalResponse.REJECT
 
         # Validate the goal request
         if goal_request.target_number <= 0:
             self.get_logger().info("Rejecting the goal.")
             return GoalResponse.REJECT
+        
+        # Policy: preempt existing goal when receiving new goal
+        with self.goal_lock_:
+            if self.goal_handle_ is not None and self.goal_handle_.is_active:
+                self.get_logger().info("Abort current goal and accept new goal")
+                self.goal_handle_.abort()
+
         self.get_logger().info("Accepting the goal")
         return GoalResponse.ACCEPT 
 
@@ -56,10 +63,15 @@ class CountUntilServerNode(Node):
         result = CountUntil.Result()
         counter = 0
         for i in range(target_number):
+            if not goal_handle.is_active:
+                result.reached_number = counter
+                return result
             if goal_handle.is_cancel_requested:
                 self.get_logger().info("Canceling the goal")
                 goal_handle.canceled()
                 result.reached_number = counter
+                with self.goal_lock_:
+                    self.goal_handle_ = None  
                 return result
             counter += 1
             self.get_logger().info(str(counter))
@@ -72,6 +84,8 @@ class CountUntilServerNode(Node):
 
         # and send the result
         result.reached_number = counter
+        with self.goal_lock_:
+            self.goal_handle_ = None  # 명시적으로 초기화
         return result
 
 def main(args=None):
