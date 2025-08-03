2d6180dda3a5343490597392a6d02e1443e626a9
maymayko9559 <maymayko9559@gmail.com>
2025-08-02 22:35:57 -0500

    test multiple goals in parallel

--- ros2_ws/src/actions_py/actions_py/count_until_client.py
+++ ros2_ws/src/actions_py/actions_py/count_until_client.py
@@ -27,7 +27,7 @@ class CountUntilClientNode(Node):
                 add_done_callback(self.goal_response_callback)
         
         # Send a cancel request 2 seconds later. 
-        self.timer_ = self.create_timer(2.0, self.cancel_goal)
+        # self.timer_ = self.create_timer(2.0, self.cancel_goal)
 
     def cancel_goal(self):
         self.get_logger().info("Send a cancel request")
@@ -65,6 +65,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = CountUntilClientNode() 
     node.send_goal(6, 1.0)
+    node.send_goal(6, 0.7)
     rclpy.spin(node)
     rclpy.shutdown()
 
