53da0b9802eaa8458308be789116ea4a2b0d94e4
maymayko9559 <maymayko9559@gmail.com>
2025-08-25 11:25:28 -0500

    single threaded executors cpp

--- ros2_ws/src/executors_cpp/src/single_threaded_executor.cpp
+++ ros2_ws/src/executors_cpp/src/single_threaded_executor.cpp
@@ -69,7 +69,13 @@ private:
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    // todo
+    auto node1 = std::make_shared<Node1>();
+    auto node2 = std::make_shared<Node2>();
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node1);
+    executor.add_node(node2);
+    executor.spin();
     rclcpp::shutdown();
     return 0;
 }
