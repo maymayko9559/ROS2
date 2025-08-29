6950bc225aa6da07975f346feb76b62aeb556657
maymayko9559 <maymayko9559@gmail.com>
2025-08-28 22:14:29 -0500

    manual composition with python.

--- ros2_ws/src/components_py/components_py/manual_composition.py
+++ ros2_ws/src/components_py/components_py/manual_composition.py
@@ -3,41 +3,8 @@ import rclpy
 from rclpy.node import Node
 import time
 from rclpy.executors import SingleThreadedExecutor
-
-class Node1(Node):
-    def __init__(self):
-        super().__init__("node1")
-        self.timer1_ = self.create_timer(1.0, self.callback_timer1)
-        self.timer2_ = self.create_timer(1.0, self.callback_timer2)
-        self.timer3_ = self.create_timer(1.0, self.callback_timer3)
-
-    def callback_timer1(self):
-        time.sleep(2.0)
-        self.get_logger().info("cb 1")
-
-    def callback_timer2(self):
-        time.sleep(2.0)
-        self.get_logger().info("cb 2")
-
-    def callback_timer3(self):
-        time.sleep(2.0)
-        self.get_logger().info("cb 3")
-
-
-class Node2(Node):
-    def __init__(self):
-        super().__init__("node2")
-        self.timer4_ = self.create_timer(1.0, self.callback_timer4)
-        self.timer5_ = self.create_timer(1.0, self.callback_timer5)
-
-    def callback_timer4(self):
-        time.sleep(2.0)
-        self.get_logger().info("cb 4")
-
-    def callback_timer5(self):
-        time.sleep(2.0)
-        self.get_logger().info("cb 5")
-
+from components_py.node1 import  Node1
+from components_py.node2 import  Node2
 
 def main(args=None):
     rclpy.init(args=args)
--- ros2_ws/src/components_py/setup.py
+++ ros2_ws/src/components_py/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "manual_composition = components_py.manual_composition:main"
         ],
     },
 )
