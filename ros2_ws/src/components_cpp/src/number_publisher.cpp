3742c05f4237ffb76e43a02c5664ff8835cc21f9
maymayko9559 <maymayko9559@gmail.com>
2025-08-30 21:47:37 -0500

    load a component at run time with ros2 component with cmd line

--- ros2_ws/src/components_cpp/src/number_publisher.cpp
+++ ros2_ws/src/components_cpp/src/number_publisher.cpp
@@ -1,39 +1,28 @@
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/msg/int64.hpp"
+#include "components_cpp/number_publisher.hpp"
 
 using namespace std::chrono_literals;
 
-class NumberPublisherNode : public rclcpp::Node
+namespace my_namespace {
+
+NumberPublisher::NumberPublisher(const rclcpp::NodeOptions &options) : Node("number_publisher", options)
 {
-public:
-    NumberPublisherNode() : Node("number_publisher")
-    {
-        number_ = 2;
+    number_ = 2;
 
-        number_publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
-        number_timer_ = this->create_wall_timer(1000ms,
-                                                std::bind(&NumberPublisherNode::publishNumber, this));
-        RCLCPP_INFO(this->get_logger(), "Number publisher has been started.");
-    }
-
-private:
-    void publishNumber()
-    {
-        auto msg = example_interfaces::msg::Int64();
-        msg.data = number_;
-        number_publisher_->publish(msg);
-    }
-
-    int number_;
-    rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr number_publisher_;
-    rclcpp::TimerBase::SharedPtr number_timer_;
-};
-
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<NumberPublisherNode>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+    number_publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
+    number_timer_ = this->create_wall_timer(1000ms,
+                                            std::bind(&NumberPublisher::publishNumber, this));
+    RCLCPP_INFO(this->get_logger(), "Number publisher has been started.");
 }
+
+void NumberPublisher::publishNumber()
+{
+    auto msg = example_interfaces::msg::Int64();
+    msg.data = number_;
+    number_publisher_->publish(msg);
+}
+
+} // namespace my_namespace
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(my_namespace::NumberPublisher)
